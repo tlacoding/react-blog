77455ee7b74ecca858938c648018efd119e07ab7
Le Pham Minh Thong <lpmthong@gmail.com>
2017-02-18 23:13:08 +0700

    Add firebase package and initialize new app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import * as firebase from 'firebase';
 
 import './styles/main.css';
 
 import routes from './routers';
 import App from './components/App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+var config = {
+  apiKey: "AIzaSyBPylmACEz_psNitIB60Yn72MqcDPIk6_o",
+  authDomain: "tlacoding.firebaseapp.com",
+  databaseURL: "https://tlacoding.firebaseio.com",
+  storageBucket: "tlacoding.appspot.com",
+  messagingSenderId: "554082963121"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <Router history={browserHistory} routes={routes} />, document.getElementById('root')
