97ec9672dcfe3f92abf75f099c0453b9c73d4c10
Le Pham Minh Thong <lpmthong@gmail.com>
2017-02-23 21:54:15 +0700

    Add route for post detail page

--- src/routers.js
+++ src/routers.js
@@ -3,9 +3,11 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import PostList from './components/PostList';
+import PostDetail from './components/PostDetail';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostList} />
+    <Route path="/post/:postId" component={PostDetail}/>
   </Route>
 );
