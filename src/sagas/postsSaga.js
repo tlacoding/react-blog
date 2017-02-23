d3f6b263d6bac97932f1b3d4c65e77596ac2f5ba
Le Pham Minh Thong <lpmthong@gmail.com>
2017-02-23 21:40:32 +0700

    Fix eslint warning '=='

--- src/sagas/postsSaga.js
+++ src/sagas/postsSaga.js
@@ -5,7 +5,7 @@ import * as types from '../constants/actionTypes';
 export function* fetchPostsSaga() {
   try {
     const response = yield call(fetchPosts);
-    if (response.type == 'success') {
+    if (response.type === 'success') {
       yield put({ type: types.FETCH_POST_SUCCESS, payload: response.payload });
     } else {
       yield put({ type: types.FETCH_POST_FAILURE, payload: response.payload });
