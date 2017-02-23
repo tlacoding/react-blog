08c83c9ffc3e507afc74ad7f7cd8c7292c6e30f5
Le Pham Minh Thong <lpmthong@gmail.com>
2017-02-23 22:17:56 +0700

    Fix action constants

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -5,7 +5,7 @@ export const FETCH_POST_FAILURE = 'FETCH_POST_FAILURE';
 
 // Fetch posts
 export const FETCH_POSTS = 'FETCH_POSTS';
-export const FETCH_POSTS_SUCCESS = 'FETCH_POST_SUCCESS';
+export const FETCH_POSTS_SUCCESS = 'FETCH_POSTS_SUCCESS';
 export const FETCH_POSTS_FAILURE = 'FETCH_POSTS_FAILURE';
 
 // Create new post
