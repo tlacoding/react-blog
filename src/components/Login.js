24d8c54fb67fd1b66689d831e695eddba7f5fa50
Le Pham Minh Thong <lpmthong@gmail.com>
2017-02-25 09:29:48 +0700

    Add login component and route for login page

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+/* eslint no-useless-constructor: "off" */
+import React, { Component, PropTypes } from 'react';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hi Dude, this is login page</h1>
+    );
+  }
+}
+
+export default Login;
--- src/routers.js
+++ src/routers.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import PostList from './components/PostList';
 import PostDetail from './components/PostDetail';
+import Login from './components/Login';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostList} />
     <Route path="/post/:postId" component={PostDetail}/>
+    <Route path="/login" component={Login}/>
   </Route>
 );
