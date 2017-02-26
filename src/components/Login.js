2c248bd60823044644f68eec578cbb367a861fcd
Le Pham Minh Thong <lpmthong@gmail.com>
2017-02-26 16:17:20 +0700

    Create Login component for login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 /* eslint no-useless-constructor: "off" */
 import React, { Component, PropTypes } from 'react';
 
+import Header from './common/Header';
+
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -8,7 +10,27 @@ class Login extends Component {
 
   render() {
     return (
-      <h1>Hi Dude, this is login page</h1>
+      <div className="row">
+        <div className="col-md-12">
+          <Header type="pageHeading" title="Hi Dude! Have a good day." subTitle="Fill your informations to login" />
+        </div>
+        <div className="col-md-6 col-md-offset-3">
+          <form>
+            <div className="form-group">
+              <label htmlFor="email">Email address:</label>
+              <input type="email" className="form-control" id="email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="pwd">Password:</label>
+              <input type="password" className="form-control" id="pwd" />
+            </div>
+            <div className="checkbox">
+              <label><input type="checkbox" /> Remember me</label>
+            </div>
+            <button type="submit" className="btn btn-default">Submit</button>
+          </form>
+        </div>
+      </div>
     );
   }
 }
