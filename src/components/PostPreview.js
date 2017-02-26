2c5a83aa740abbcbe461daa893cc07af258d3eea
Le Pham Minh Thong <lpmthong@gmail.com>
2017-02-26 16:16:33 +0700

    Update tag a to Link

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 import { formatPostDate } from '../ultils';
 
 const PostPreview = ({ post }) => {
   return (
     <article className="post-preview">
-      <a href={ "/post/" + post.key }>
+      <Link to={ "/post/" + post.key }>
         <h2 className="post-title">{ post.title }</h2>
-      </a>
+      </Link>
       <p className="post-meta">Posted on { formatPostDate(post.created) }
       </p>
 
       <div className="post-entry">{ post.summary }</div>
-      <a href={ "/post/" + post.key } className="btn btn-default btn-default">Read More</a>
+      <Link to={ "/post/" + post.key } className="btn btn-default btn-default">Read More</Link>
     </article>
   );
 };
